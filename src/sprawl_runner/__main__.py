cd06de6c993b3063776d95efedf9e1e2758458cf
Robert Groves <rwgdev@gmail.com>
2024-03-18 12:33:21 -0500

    Exclude __main__.py from tests

--- src/sprawl_runner/__main__.py
+++ src/sprawl_runner/__main__.py
@@ -1,3 +1,12 @@
+"""
+This is the entry point used when run as a module (via python -m sprawl_runner).
+
+Note this should never do anything more than call the main function. This file
+is being excluded from automated testing based on the assumption that the core
+logic executed is tested via other means; in this case the main function
+invoked here has its own set of tests.
+"""
+
 from sprawl_runner.main import main
 
 # Call main if this is run as a module.
